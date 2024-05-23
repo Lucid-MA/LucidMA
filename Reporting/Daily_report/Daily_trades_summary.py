c1cdb8ddbec4358b794e89abead5b12894930de7
Tony <tony.hoang@lucidma.com>
2024-05-23 16:58:57 -0400

    update email recipients

--- Reporting/Daily_report/Daily_trades_summary.py
+++ Reporting/Daily_report/Daily_trades_summary.py
@@ -12,25 +12,25 @@ from Utils.SQL_queries import (
 )
 from Utils.database_utils import execute_sql_query
 
-# # Custom run date
-valdate = "2024-05-22"
+# Custom run date
+# valdate = "2024-05-22"
 
-# Get the current date and format it
+# # Get the current date and format it
 current_date = datetime.now().strftime("%Y-%m-%d")
 valdate = current_date
 
 recipients = [
     "tony.hoang@lucidma.com",
-    # "Heather.Campbell@lucidma.com",
-    # "operations@lucidma.com",
+    "Heather.Campbell@lucidma.com",
+    "operations@lucidma.com",
 ]
 
 recipients_mmt = [
     "tony.hoang@lucidma.com",
-    # "Heather.Campbell@lucidma.com",
-    # "martin.stpierre@lucidma.com",
-    # "mattias.almers@lucidma.com",
-    # "david.carlson@lucidma.com",
+    "Heather.Campbell@lucidma.com",
+    "martin.stpierre@lucidma.com",
+    "mattias.almers@lucidma.com",
+    "david.carlson@lucidma.com",
 ]
 
 df_helix_current_trade = execute_sql_query(
