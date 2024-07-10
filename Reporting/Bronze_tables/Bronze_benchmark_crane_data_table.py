52f28e2a01230abd7af6ae62d3c968f5db8b6faf
Tony Hoang <tony@Tonys-MacBook-Pro.local>
2024-07-10 17:15:14 -0400

    crane data updated

--- Reporting/Bronze_tables/Bronze_benchmark_crane_data_table.py
+++ Reporting/Bronze_tables/Bronze_benchmark_crane_data_table.py
@@ -32,10 +32,76 @@ crane_data_df = pd.read_excel(
 )
 
 # Remove rows with invalid "Date" column
-crane_data_df = crane_data_df[crane_data_df["Date"].notna()]
+crane_data_df = crane_data_df[
+    (crane_data_df["Date"].notna()) & (crane_data_df["Date"] != "0-Jan-00")
+]
+
+
+# Function to check if a date can be converted to datetime
+def is_valid_date(date):
+    try:
+        pd.to_datetime(date)
+        return True
+    except:
+        return False
+
+
+# Filter out rows with dates that can't be converted
+crane_data_df = crane_data_df[crane_data_df["Date"].apply(is_valid_date)]
+
+# Now you can safely convert the remaining dates
+crane_data_df["Date"] = pd.to_datetime(crane_data_df["Date"]).dt.strftime("%Y-%m-%d")
 
 # Add the "timestamp" column
 crane_data_df["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Define the prefixes for column names
+prefixes = [
+    "CRANE MONEY FUND AVERAGE",
+    "CRANE 100 MONEY FUND INDEX",
+    "CRANE INSTITUTIONAL MF INDX",
+    "CRANE RETAIL MF INDEX",
+    "CRANE TREAS INSTIT MF INDEX",
+    "CRANE GOVT INSTIT MF INDEX",
+    "CRANE PRIME INSTIT MF INDEX",
+    "CRANE TREAS RETAIL MF INDEX",
+    "CRANE GOVT RETAIL MF INDEX",
+    "CRANE PRIME RETAIL MF INDEX",
+    "CRANE TAX EXEMPT INDEX",
+]
+
+# Generate the column names dynamically
+column_names = ["Date", "Assets", "Fund Count"]
+for prefix in prefixes:
+    for column in [
+        "Assets",
+        "WAM",
+        "WAL",
+        "1Day%",
+        "7Day%",
+        "30Day%",
+        "Gr1Day%",
+        "Gr7Day%",
+        "Gr30Day%",
+        "Exp%",
+        "Net Flow",
+        "Inflows",
+        "Outflows",
+        "NAV",
+        "DLA",
+        "WLA",
+        "Fund Count",
+    ]:
+        column_names.append(f"{prefix}_{column}")
+column_names.append("timestamp")
+
+# Replace spaces and special characters with underscores in the DataFrame column names
+column_names = [
+    col.replace(" ", "_").replace("%", "").replace("/", "_").replace(".", "_")
+    for col in column_names
+]
+
+crane_data_df.columns = column_names
 
 # Create a table schema
 table_name = "bronze_benchmark_crane_data"
@@ -44,16 +110,7 @@ crane_data_table = Table(
     table_name,
     metadata,
     Column("Date", String, primary_key=True),
-    *[
-        Column(
-            column.replace(" ", "_")
-            .replace("%", "")
-            .replace("/", "_")
-            .replace(".", "_"),
-            String,
-        )
-        for column in crane_data_df.columns[1:-1]
-    ],
+    *[Column(column_name, String) for column_name in column_names[1:-1]],
     Column("timestamp", DateTime),
 )
 
@@ -64,14 +121,6 @@ metadata.create_all(engine)
 with engine.connect() as connection:
     try:
         with connection.begin():
-            # Replace spaces and special characters with underscores in the DataFrame column names
-            crane_data_df.columns = [
-                col.replace(" ", "_")
-                .replace("%", "")
-                .replace("/", "_")
-                .replace(".", "_")
-                for col in crane_data_df.columns
-            ]
 
             # Construct the INSERT statement dynamically
             column_names = ", ".join([f'"{col}"' for col in crane_data_df.columns])
