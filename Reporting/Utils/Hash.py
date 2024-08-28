bdf677a042da076acfac113470fc9f4a27f3f9a9
Tony <tony.hoang@lucidma.com>
2024-08-28 12:13:44 -0400

    change hash function

--- Reporting/Utils/Hash.py
+++ Reporting/Utils/Hash.py
@@ -8,9 +8,12 @@ def hash_string(string):
     return decimal_value  # Convert first 12 hex digits
 
 
-def hash_string_v2(string):
+def hash_string_v2(string) -> str:
     hash_object = hashlib.sha256(string.encode("utf-8"))  # SHA-256 hash
     hex_digest = hash_object.hexdigest()
     decimal_value = int(hex_digest[:12], 16)  # Convert first 12 hex digits to decimal
     truncated_value = decimal_value % 10**12  # Truncate to 12 digits
-    return truncated_value
+    string_value = str(truncated_value).zfill(
+        12
+    )  # Convert to string and pad with leading zeros
+    return string_value
