4cad0886d931633efe4d77474eea08c6614031be
Tony <tony.hoang@lucidma.com>
2024-08-29 09:48:27 -0400

    test

--- Reporting/Draft.py
+++ Reporting/Draft.py
@@ -1,6 +1,41 @@
-from Daily_report.Daily_trades_summary import get_helix_trades
+import pandas as pd
+
 from Utils.Common import print_df
-from Utils.SQL_queries import current_trade_daily_report_helix_trade_query
+from Utils.SQL_queries import (
+    current_trade_daily_report_helix_trade_query,
+    as_of_trade_daily_report_helix_trade_query,
+)
+from Utils.database_utils import execute_sql_query
+
+
+def get_helix_trades(query, params):
+    df_helix_trade = execute_sql_query(query, "sql_server_1", params=params)
+    helix_cols = [
+        "Fund",
+        "Series",
+        "Trade ID",
+        "Issue Description",
+        "TradeType",
+        "Trade Date",
+        "Start Date",
+        "End Date",
+        "Money",
+        "Counterparty",
+        "Orig. Rate",
+        "Orig. Price",
+        "HairCut",
+        "Spread",
+        "BondID",
+        "Status",
+        "Par/Quantity",
+        "Market Value",
+        "Comments",
+        "User",
+        "Entry Time",
+    ]
+    df_helix_trade = df_helix_trade[helix_cols]
+    return df_helix_trade
+
 
 valdate = "2024-08-28"
 
@@ -10,3 +45,21 @@ df_helix_current_trade = get_helix_trades(
 )
 
 print_df(df_helix_current_trade)
+
+df_helix_as_of_trade = get_helix_trades(
+    as_of_trade_daily_report_helix_trade_query, (valdate,)
+)
+
+print_df(df_helix_as_of_trade)
+
+
+# Combine the data from df_helix_current_trade and df_helix_as_of_trade where status is 15
+df_helix_failed_to_transmitted_trade = pd.concat(
+    [
+        df_helix_current_trade[df_helix_current_trade["Status"] == 15],
+        df_helix_as_of_trade[df_helix_as_of_trade["Status"] == 15],
+    ],
+    ignore_index=True,
+)
+
+print_df(df_helix_failed_to_transmitted_trade)
