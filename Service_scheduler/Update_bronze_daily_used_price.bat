df6f46cb72b740323a744485757cd8de91cf7510
Tony Hoang <tony@Tonys-MacBook-Pro.local>
2024-09-24 17:54:40 -0400

    update bat file

--- Service_scheduler/Update_bronze_daily_used_price.bat
+++ Service_scheduler/Update_bronze_daily_used_price.bat
@@ -1,11 +1,18 @@
+@echo off
 cd /d "S:\Users\THoang\Tech\LucidMA"
 git pull
 
-cd /d "S:\Users\THoang\Tech\LucidMA\Reporting\Bronze_tables\Price"
-"S:\Users\THoang\Tech\LucidMA\venv\Scripts\python.exe" "Bronze_daily_used_prices_table.py"
+echo Current PYTHONPATH before: %PYTHONPATH%
+set PYTHONPATH=%PYTHONPATH%;S:\Users\THoang\Tech\LucidMA
+echo Modified PYTHONPATH: %PYTHONPATH%
 
-cd /d "S:\Users\THoang\Tech\LucidMA\BatchLogs"
-echo (%date% ; "used_prices_table")>>BatchLogs.txt
+cd "S:\Users\THoang\Tech\LucidMA\Reporting\Bronze_tables\Price"
+"S:\Users\THoang\Tech\LucidMA\Reporting\venv\Scripts\python.exe" "Bronze_daily_used_prices_table.py"
+
+echo PYTHONPATH after script: %PYTHONPATH%
+
+cd "S:\Users\THoang\Tech\LucidMA\BatchLogs"
+echo %date% ; "used_prices_table">>BatchLogs.txt
 
 cd /d "S:\Users\THoang\Tech\LucidMA"
 git add .
