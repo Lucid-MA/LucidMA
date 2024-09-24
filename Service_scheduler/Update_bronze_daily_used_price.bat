322ea81ddf344e367791e191506ae2b7488d7020
Tony Hoang <tony@Tonys-MacBook-Pro.local>
2024-09-24 17:58:35 -0400

    update batch file

--- Service_scheduler/Update_bronze_daily_used_price.bat
+++ Service_scheduler/Update_bronze_daily_used_price.bat
@@ -6,7 +6,7 @@ set PYTHONPATH=%PYTHONPATH%;S:\Users\THoang\Tech\LucidMA
 echo Modified PYTHONPATH: %PYTHONPATH%
 
 cd "S:\Users\THoang\Tech\LucidMA\Reporting\Bronze_tables\Price"
-"S:\Users\THoang\Tech\LucidMA\Reporting\venv\Scripts\python.exe" "Bronze_daily_used_prices_table.py"
+S:\Users\THoang\Tech\LucidMA\Reporting\venv\Scripts\python.exe "Bronze_daily_used_prices_table.py"
 
 echo PYTHONPATH after script: %PYTHONPATH%
 
