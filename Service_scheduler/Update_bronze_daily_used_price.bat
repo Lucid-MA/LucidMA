aa59188b50f780af01e54764c894cf53f5cd2b1c
Tony Hoang <tony@Tonys-MacBook-Pro.local>
2024-09-24 17:44:40 -0400

    update bat file

--- Service_scheduler/Update_bronze_daily_used_price.bat
+++ Service_scheduler/Update_bronze_daily_used_price.bat
@@ -2,7 +2,7 @@ cd /d "S:\Users\THoang\Tech\LucidMA"
 git pull
 
 cd /d "S:\Users\THoang\Tech\LucidMA\Reporting\Bronze_tables\Price"
-"S:\Users\THoang\Tech\LucidMA\Reporting\venv\Scripts\python.exe" "Bronze_daily_used_prices_table.py"
+"S:\Users\THoang\Tech\LucidMA\venv\Scripts\python.exe" "Bronze_daily_used_prices_table.py"
 
 cd /d "S:\Users\THoang\Tech\LucidMA\BatchLogs"
 echo (%date% ; "used_prices_table")>>BatchLogs.txt
