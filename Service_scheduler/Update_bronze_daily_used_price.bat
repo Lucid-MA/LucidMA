a2aad1fd90e2e1f29109cd3056764374ddbe3627
Tony <tony.hoang@lucidma.com>
2024-09-24 22:03:08 -0400

    update batch file

--- Service_scheduler/Update_bronze_daily_used_price.bat
+++ Service_scheduler/Update_bronze_daily_used_price.bat
@@ -1,19 +1,42 @@
 cd /d "S:\Users\THoang\Tech\LucidMA"
+
+:: Pull the latest changes from the repository
 git pull
+
+if %ERRORLEVEL% neq 0 (
+    echo Git pull failed. Exiting script.
+    exit /b 1
+)
 
 echo Current PYTHONPATH before: %PYTHONPATH%
 set PYTHONPATH=%PYTHONPATH%;S:\Users\THoang\Tech\LucidMA
 echo Modified PYTHONPATH: %PYTHONPATH%
 
 cd "S:\Users\THoang\Tech\LucidMA\Reporting\Bronze_tables\Price"
-S:\Users\THoang\Tech\LucidMA\Reporting\venv\Scripts\python.exe "Bronze_daily_used_prices_table.py"
+"S:\Users\THoang\Tech\LucidMA\venv\Scripts\python.exe" "Bronze_daily_used_prices_table.py"
+if %ERRORLEVEL% neq 0 (
+    echo Python script execution failed. Exiting script.
+    exit /b 1
+)
 
 echo PYTHONPATH after script: %PYTHONPATH%
 
+
+:: Navigate to the logs directory and append the date to the log file
 cd "S:\Users\THoang\Tech\LucidMA\BatchLogs"
 echo %date% ; "used_prices_table">>BatchLogs.txt
 
+:: Add the log changes to git, commit, and push them
 cd /d "S:\Users\THoang\Tech\LucidMA"
-git add .
-git commit -m "update daily price %date%"
+git add BatchLogs.txt
+git commit -m "Update daily price log on %date%"
+if %ERRORLEVEL% neq 0 (
+    echo Git commit failed. Exiting script.
+    exit /b 1
+)
+
 git push
+if %ERRORLEVEL% neq 0 (
+    echo Git push failed. Exiting script.
+    exit /b 1
+)
