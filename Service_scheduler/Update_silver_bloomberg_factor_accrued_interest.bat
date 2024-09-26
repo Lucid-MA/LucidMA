7377af74b747d5deb9640c29be0c6fd0b47fdc1b
Tony Hoang <tony@Tonys-MacBook-Pro.local>
2024-09-26 19:13:09 -0400

    update bat

--- Service_scheduler/Update_silver_bloomberg_factor_accrued_interest.bat
+++ Service_scheduler/Update_silver_bloomberg_factor_accrued_interest.bat
@@ -21,10 +21,23 @@ if %ERRORLEVEL% neq 0 (
 cd "S:\Users\THoang\Tech\LucidMA\Service_scheduler\BatchLogs"
 echo %date% %time% ; "silver_bloomberg_factor_interest_accrued_table - Script execution: %status%">>BatchLogs.txt
 
+cd "S:\Users\THoang\Tech\LucidMA\Reporting\Silver_tables"
+"S:\Users\THoang\Tech\LucidMA\venv\Scripts\python.exe" "Silver_clean_and_dirty_prices_table.py"
+if %ERRORLEVEL% neq 0 (
+    set "status=Failed"
+    echo Python script execution failed.
+) else (
+    set "status=Success"
+)
+
+:: Navigate to the logs directory and append the date, time, and status to the log file
+cd "S:\Users\THoang\Tech\LucidMA\Service_scheduler\BatchLogs"
+echo %date% %time% ; "silver_clean_and_dirty_price - Script execution: %status%">>BatchLogs.txt
+
 :: Add the log changes to git, commit, and push them
 cd /d "S:\Users\THoang\Tech\LucidMA"
 git add BatchLogs.txt
-git commit -m "Update daily factor and interest accrued log on %date%"
+git commit -m "Update daily factor, interest, clean, dirty prices log on %date%"
 if %ERRORLEVEL% neq 0 (
     echo Git commit failed. Exiting script.
     exit /b 1
