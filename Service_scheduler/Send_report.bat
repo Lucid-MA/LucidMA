122099a0f27d3cf38e14ccd3f3e6a4eefa2d5876
Tony Hoang <tony@Tonys-MacBook-Pro.local>
2024-11-01 16:58:05 -0400

    update report

---
+++ Service_scheduler/Send_report.bat
@@ -0,0 +1,18 @@
+cd /d "S:\Users\THoang\Tech\LucidMA"
+
+echo Current PYTHONPATH before: %PYTHONPATH%
+set PYTHONPATH=%PYTHONPATH%;S:\Users\THoang\Tech\LucidMA
+echo Modified PYTHONPATH: %PYTHONPATH%
+
+cd "S:\Users\THoang\Tech\LucidMA\Reporting\Daily_report"
+"S:\Users\THoang\Tech\LucidMA\venv\Scripts\python.exe" "Trade_allocations_report.py"
+if %ERRORLEVEL% neq 0 (
+    set "status=Failed"
+    echo Python script execution failed.
+) else (
+    set "status=Success"
+)
+
+:: Navigate to the logs directory and append the date, time, and status to the log file
+cd "S:\Users\THoang\Tech\BatchLogs"
+echo %date% %time% ; "used_prices_table - Script execution: %status%">>BatchLogs.txt
