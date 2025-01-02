0592bac505722e0d2fa86d4c497859642c023a8d
Tony <tony.hoang@lucidma.com>
2025-01-02 11:49:22 -0500

    update

--- Reporting/Form PF/generate_form_pf_xml.py
+++ Reporting/Form PF/generate_form_pf_xml.py
@@ -31,7 +31,7 @@ FILING_DATE = "2024-12-31"  # quarter-end here as YYYY-MM-DD string
 IS_QUARTERLY_FILING = True
 
 # Turn this flag on to get data for quarterly only
-ONLY_QUARTERLY_DATA = False
+ONLY_QUARTERLY_DATA = True
 
 DOING_HEDGE = False  # if y/e and doing MMT and other non liq-funds
 # WORKBOOK_PATH = prefix_path + "2024/10.15.24/Lucid Form PF Q3 - Updated for Part 3 Amendments.xlsx"
