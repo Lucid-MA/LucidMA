906b932ea061f47c0dbbc94ee55b1e1d883b4ae9
Tony Hoang <tony@Tonys-MacBook-Pro.local>
2025-02-06 22:15:42 -0500

    bond_data_factor

--- Reporting/Bronze_tables/Bronze_bond_data_factor_table.py
+++ Reporting/Bronze_tables/Bronze_bond_data_factor_table.py
@@ -4,12 +4,17 @@ import re
 from datetime import datetime
 
 import pandas as pd
-from sqlalchemy import Table, MetaData, Column, String, Date, inspect
+from sqlalchemy import Table, MetaData, Column, String, Date, inspect, DateTime
 from sqlalchemy.exc import SQLAlchemyError
 
 from Reporting.Utils.Hash import hash_string_v2
 from Reporting.Utils.database_utils import engine_prod, upsert_data
-from Utils.Common import get_repo_root
+from Utils.Common import (
+    get_repo_root,
+    get_file_path,
+    get_current_timestamp_datetime,
+    get_current_timestamp,
+)
 
 # Configure logger
 logger = logging.getLogger(__name__)
@@ -17,23 +22,27 @@ logger = logging.getLogger(__name__)
 # Configurations
 PUBLISH_TO_PROD = True
 table_name = "bronze_bond_data_factor"
-directory = r"S:/Lucid/Data/Bond Data/Historical"
+directory = get_file_path(r"S:/Lucid/Data/Bond Data/Historical")
 # Get the repository root directory
 repo_path = get_repo_root()
 bronze_tracker_dir = repo_path / "Reporting" / "Bronze_tables" / "File_trackers"
 
-processed_files_tracker =(bronze_tracker_dir / "Bronze Table Processed Daily Factor Data")
-skipped_files_tracker = (bronze_tracker_dir / "Bronze Table Skipped Daily Factor Data")
+processed_files_tracker = (
+    bronze_tracker_dir / "Bronze Table Processed Daily Factor Data"
+)
+skipped_files_tracker = bronze_tracker_dir / "Bronze Table Skipped Daily Factor Data"
 
 # File patterns and required columns
 file_patterns = [
     r"Bond Data-\d{4}-\d{2}-\d{2}\.xlsm",
-    r"Bond_Data_\d{2}_\d{2}_\d{4}_PM\.xlsx"
+    r"Bond_Data_\d{2}_\d{2}_\d{4}_PM\.xlsx",
 ]
-required_columns = ['CUSIP', 'SECURITY_TYP', 'ISSUER', 'MTG Factor', 'Int Acc']
+required_columns = ["CUSIP", "SECURITY_TYP", "ISSUER", "MTG Factor", "Int Acc"]
 
 # SQLAlchemy Engine
-engine = engine_prod if PUBLISH_TO_PROD else None  # Replace with your staging engine if needed
+engine = (
+    engine_prod if PUBLISH_TO_PROD else None
+)  # Replace with your staging engine if needed
 
 
 def initialize_table(table_name):
@@ -45,13 +54,13 @@ def initialize_table(table_name):
         metadata,
         Column("data_id", String(255), primary_key=True),
         Column("bond_data_date", Date),
-        Column("CUSIP", String),
-        Column("SECURITY_TYP", String),
-        Column("ISSUER", String),
-        Column("MTG Factor", String),
-        Column("Int Acc", String),
-        Column("File_date", Date),
-        Column("File_name", String),
+        Column("bond_id", String),
+        Column("security_type", String),
+        Column("issuer", String),
+        Column("mtg_factor", String),
+        Column("interest_accrued", String),
+        Column("file_name", String),
+        Column("timestamp", DateTime),
         extend_existing=True,
     )
     metadata.create_all(engine)
@@ -59,14 +68,18 @@ def initialize_table(table_name):
 
 
 def extract_date_from_filename(filename):
-    """Extract the date from a file name."""
+    """Extract the date from a file name and return in MM-DD-YYYY format."""
     date_match = re.search(r"(\d{4}-\d{2}-\d{2})", filename)
     if date_match:
-        return datetime.strptime(date_match.group(), "%Y-%m-%d")
-    date_match = re.search(r"(\d{2}_\d{2}_\d{4})", filename)
-    if date_match:
-        return datetime.strptime(date_match.group(), "%m_%d_%Y")
-    return None
+        date_obj = datetime.strptime(date_match.group(), "%Y-%m-%d")
+    else:
+        date_match = re.search(r"(\d{2}_\d{2}_\d{4})", filename)
+        if date_match:
+            date_obj = datetime.strptime(date_match.group(), "%m_%d_%Y")
+        else:
+            return None  # No match found
+
+    return date_obj.strftime("%m-%d-%Y")  # Convert to MM-DD-YYYY format
 
 
 def read_tracker(file_path):
@@ -96,12 +109,24 @@ def process_file(filepath, filename, processed_files):
         # Read the Excel file
         df = pd.read_excel(filepath, header=4)
         df = df.loc[7:, required_columns]
-        df['File_date'] = file_date
-        df['File_name'] = filename
-
+        df["File_name"] = filename
+        df["bond_data_date"] = file_date
         # Add a data_id column
-        df['data_id'] = df.apply(
+        df["data_id"] = df.apply(
             lambda row: hash_string_v2(f"{row['CUSIP']}{filename}"), axis=1
+        )
+        df["timestamp"] = get_current_timestamp()
+
+        df.rename(
+            columns={
+                "CUSIP": "bond_id",
+                "SECURITY_TYP": "security_type",
+                "ISSUER": "issuer",
+                "MTG Factor": "mtg_factor",
+                "Int Acc": "interest_accrued",
+                "File_name": "file_name",
+            },
+            inplace=True,
         )
 
         # Upsert data into the table
