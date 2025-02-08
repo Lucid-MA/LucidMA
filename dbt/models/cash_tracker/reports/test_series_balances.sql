2208b8b14980a825bef8e8c9a6d4aad4e12e3d46
John Turner <john.d.turner@me.com>
2025-02-07 19:41:06 -0500

    Add model for series vs master balance test

---
+++ dbt/models/cash_tracker/reports/test_series_balances.sql
@@ -0,0 +1,50 @@
+WITH 
+master_summary AS (
+  SELECT
+    *
+  FROM {{ ref('cash_tracker__balance_summary') }}
+  WHERE report_date >= '2024-10-31'
+  AND fund = 'PRIME'
+  AND acct_name IN ('MAIN','MARGIN','MANAGEMENT')
+),
+series_balance_history AS (
+  SELECT
+    *
+  FROM {{ ref('cash_tracker__balance_history_series') }}
+  WHERE fund = 'PRIME'
+),
+series_balance_summary AS (
+  SELECT
+    *
+  FROM {{ ref('cash_tracker__balance_summary_series') }}
+  WHERE fund = 'PRIME'
+),
+starting_balance AS (
+  SELECT
+    *
+  FROM {{ ref('series_balance') }}
+  WHERE balance_date = '2024-10-31'
+  AND fund = 'PRIME'
+  AND series = 'MASTER'
+),
+final AS (
+  SELECT
+    sh.*,
+    CAST(SUM(sh.series_cash_eod) OVER (PARTITION BY sh.report_date, sh.fund, sh.flow_account) 
+    AS MONEY) AS series_cash_total,
+    ms.cash_actual_eod,
+    CAST(SUM(sh.series_sweep_eod) OVER (PARTITION BY sh.report_date, sh.fund, sh.flow_account) 
+    AS MONEY) AS series_sweep_total,
+    ms.sweep_actual_eod
+  FROM series_balance_history AS sh
+  LEFT JOIN master_summary AS ms 
+    ON (
+      sh.report_date = ms.report_date
+      AND sh.fund = ms.fund
+      AND sh.flow_account = ms.acct_name
+    )
+)
+
+SELECT 
+  *
+FROM final
